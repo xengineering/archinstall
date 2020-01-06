a2d153618340705c9553a493ca90008469f97e94
xengineering <mail2xengineering@protonmail.com>
2020-01-06 16:33:33 +0100

    Unnecessary hotfix has been removed.

--- util/install_packages.sh
+++ util/install_packages.sh
@@ -21,7 +21,7 @@
 desktop_wanted=$1  # e.g. "yes"
 
 
-pacstrap -G /mnt base linux linux-firmware dhcpcd nano sudo grub efibootmgr
+pacstrap /mnt base linux linux-firmware dhcpcd nano sudo grub efibootmgr
 if [ "$desktop_wanted" = "yes" ]; then
     pacstrap /mnt xorg lightdm lightdm-gtk-greeter light-locker accountsservice gnome-screensaver xfce4-pulseaudio-plugin network-manager-applet xfce4 mousepad
 fi
