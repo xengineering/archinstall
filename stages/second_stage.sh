b1157730870955e662154816ea3450117b45d8ee
xengineering <mail2xengineering@protonmail.com>
2020-01-21 19:22:19 +0100

    Bugfix: Configure Initramfs and Desktop just if needed

--- stages/second_stage.sh
+++ stages/second_stage.sh
@@ -29,10 +29,14 @@ bash configure_timezone.sh /usr/share/zoneinfo/Europe/Berlin
 
 bash configure_network.sh $hostname
 
-bash configure_initramfs.sh
+if [ $system_encryption == "yes" ]; then
+    bash configure_initramfs.sh
+fi
 
 bash configure_users.sh $admin_username $DEFAULT_PASSWORD
 
 bash install_bootloader.sh $efi_partition_path $system_encryption $main_partition_path
 
-bash configure_desktop.sh
+if [ "$desktop_wanted" = "yes" ]; then
+    bash configure_desktop.sh $desktop
+fi
